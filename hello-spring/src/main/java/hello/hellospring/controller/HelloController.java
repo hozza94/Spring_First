4dc33405b661e021fccf6a037a2dcff05c724ddd
hozza94 <hozza94@users.noreply.github.com>
2020-08-10 22:17:02 +0900

    1016

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 
@@ -12,5 +14,17 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data","hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // 뷰 문자가 그대로 날아감? 소스코드 보기하면 알수있음
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
     }
 }
